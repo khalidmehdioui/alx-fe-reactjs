7ae7970bf93227fcbf5ba917f165d083e2508671
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-11 22:35:37 +0100

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,8 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <h1>Welcome to ALX React APP!</h1>
             <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
         </div>
     );
 }
