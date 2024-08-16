0a0e6a836cf4319c0491606f9ab9ac864794032f
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-16 21:12:38 +0100

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer>
-        <p style={{color : 'violet' ,border:'1px dotted' }}>© 2023 City Lovers</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
