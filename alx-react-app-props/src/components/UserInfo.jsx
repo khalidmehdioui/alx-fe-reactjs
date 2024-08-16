02e68a03f3a6286f82a9cbee2f302fde44806ad4
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-16 21:39:24 +0100

    Add files via upload

---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,13 @@
+import UserDetails from './UserDetails';
+import UserProfile from './UserProfile';
+
+function UserInfo() {
+  return (
+  <div>
+    <UserDetails />
+    <UserProfile />
+    </div>
+    )
+}
+
+export default UserInfo;
