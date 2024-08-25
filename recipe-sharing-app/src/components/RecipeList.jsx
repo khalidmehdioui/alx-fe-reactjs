a9f3c13aab98bc18f7bb766b112339843c50b695
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 22:36:27 +0100

    Add files via upload

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import useRecipeStore from './recipeStore';
+import { Link } from 'react-router-dom';
+
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+
+  return (
+    <div>
+      {filteredRecipes.length === 0 ? (
+        <p>No recipes found</p>
+      ) : (
+        filteredRecipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
