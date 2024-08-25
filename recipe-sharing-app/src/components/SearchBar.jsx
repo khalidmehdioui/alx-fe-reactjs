fa9e106c56ffb88596f26527107d922b38519929
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 13:44:03 +0100

    Update SearchBar.jsx

--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -1,22 +1,14 @@
-// src/components/SearchBar.jsx
-//import React from 'react';
-import { useRecipeStore } from '../recipeStore';
+import React from 'react';
+import useRecipeStore from './recipeStore';
 
 const SearchBar = () => {
   const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-  const filterRecipes = useRecipeStore(state => state.filterRecipes);
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setSearchTerm(value);
-    filterRecipes();
-  };
 
   return (
     <input
       type="text"
       placeholder="Search recipes..."
-      onChange={handleChange}
+      onChange={(e) => setSearchTerm(e.target.value)}
     />
   );
 };
