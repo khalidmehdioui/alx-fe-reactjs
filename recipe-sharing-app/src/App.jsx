5a450fe96f35385868947726615a8923a4753831
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 21:19:45 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+
 function App() {
   return (
     <Router>
-       <div>
-        <h1>Recipe Sharing App</h1>
+      <div>
+        <h1>Recipe Sharing Application</h1>
         <SearchBar />
-        <AddRecipeForm />
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
-        <RecipeList />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-      <div>
+    </Router>
+  );
+}
+
+export default App;
         <h1>Recipe Sharing App</h1>
         <SearchBar />
         <AddRecipeForm />
