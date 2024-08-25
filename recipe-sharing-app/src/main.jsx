031afbcf031ce6186412daf5e611260b12d8f386
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 19:28:31 +0100

    Update main.jsx

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import RecipeDetails from './components/RecipeDetails';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+    </Routes>
+  </Router>
+);
