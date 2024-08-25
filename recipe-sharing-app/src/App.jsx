ac6084b385073bea5e51a3441bdad301996b6fc6
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 22:41:25 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,41 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
+import EditRecipeForm from './components/EditRecipeForm';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
+
 function App() {
   return (
     <Router>
-       <div>
+      <div className="App">
         <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <AddRecipeForm />
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
-        <RecipeList />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <AddRecipeForm />
-        <FavoritesList />
-        <RecommendationsList />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/editrecipefrom" element={<EditRecipeForm />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
         </Routes>
       </div>
     </Router>
@@ -43,3 +26,4 @@ function App() {
 }
 
 export default App;
+
