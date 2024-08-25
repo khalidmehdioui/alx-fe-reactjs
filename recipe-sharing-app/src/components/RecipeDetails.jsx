ab1be49ca8338cb854491b6da3f57530f9a325dc
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 22:10:59 +0100

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { useRecipeStore } from './recipeStore';
 import { useParams } from 'react-router-dom';
-import useRecipeStore from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton.jsx';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeDetails = () => {
-  const { recipeId } = useParams();
+  const { id } = useParams(); // الحصول على معرف الوصفة من URL
   const recipe = useRecipeStore(state =>
-    state.recipes.find(recipe => recipe.id === parseInt(recipeId))
+    state.recipes.find(recipe => recipe.id === id)
   );
 
-  if (!recipe) return <p>Recipe not found</p>;
+  if (!recipe) {
+    return <div>الوصفة غير موجودة</div>;
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
       <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton recipeId={recipe.id} />
+      <DeleteRecipeButton recipeId={id} />
     </div>
   );
 };
