b5f9971ce12d81ab3edd8d7fd0d1f06244aada5b
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 13:35:50 +0100

    Add files via upload

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,33 @@
+// src/components/RecipeList.jsx
+import  { useEffect } from 'react';
+import { useRecipeStore } from '../recipeStore';
+import SearchBar from './SearchBar';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.filteredRecipes);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  useEffect(() => {
+    filterRecipes();
+  }, [filterRecipes]);
+
+  return (
+    <div>
+      <SearchBar />
+      <div>
+        {recipes.length > 0 ? (
+          recipes.map(recipe => (
+            <div key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>No recipes found</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeList;
