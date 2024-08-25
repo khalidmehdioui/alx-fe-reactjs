3f4ae564b90964fe35ba2f6f60a64ff11beb9042
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-25 21:20:48 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,43 +1,31 @@
-import React from 'react';
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
-import RecipeList from './components/RecipeList';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
-import AddRecipeForm from './components/AddRecipeForm';
+
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+// import EditRecipeForm from './components/EditRecipeForm'
 
 function App() {
+  
+
   return (
     <Router>
       <div>
         <h1>Recipe Sharing Application</h1>
-        <SearchBar />
         <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/favorites" element={<FavoritesList />} />
-          <Route path="/recommendations" element={<RecommendationsList />} />
-          <Route path="/add" element={<AddRecipeForm />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/" element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+            <SearchBar />
+          </>} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <AddRecipeForm />
-        <FavoritesList />
-        <RecommendationsList />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+export default App
