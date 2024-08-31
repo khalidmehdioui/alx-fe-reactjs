d4a043451d91072f7894accaf300a5ecc7adf524
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-08-31 13:46:49 +0100

    Add files via upload

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return response.data;
+};
+
+const PostsComponent = () => {
+  const { data, isLoading, isError, error, refetch } = useQuery(['posts'], fetchPosts, {
+    staleTime: 60000, 
+    cacheTime: 300000, 
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
