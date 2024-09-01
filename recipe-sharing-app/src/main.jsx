d867253c7e350f6f5ec3efd17368e2ec6466c99c
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-01 15:13:40 +0100

    Update main.jsx

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import RecipeDetails from './components/RecipeDetails';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
     </Routes>
   </Router>
 );
