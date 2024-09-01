dac22fce4664a96bf769828cb14080b69c203694
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-01 15:09:35 +0100

    Update FavoritesList.jsx

--- recipe-sharing-app/src/components/FavoritesList.jsx
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { useRecipeStore } from './recipeStore';
+import { useRecipeStore } from '../recipeStore';
 
 const FavoritesList = () => {
-  const favorites = useRecipeStore(state => state.favorites.map(id =>
-    state.recipes.find(recipe => recipe.id === id)
-  ));
+  const favorites = useRecipeStore((state) =>
+    state.favorites.map((id) =>
+      state.recipes.find((recipe) => recipe.id === id)
+    )
+  );
 
   return (
     <div>
       <h2>My Favorites</h2>
-      {favorites.length === 0 ? (
-        <p>No favorite recipes yet</p>
-      ) : (
-        favorites.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      )}
+      {favorites.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
