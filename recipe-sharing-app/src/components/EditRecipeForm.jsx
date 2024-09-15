6f04956b009185429be2544d8c753fc694a09105
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-15 12:17:16 +0100

    Update EditRecipeForm.jsx

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useRecipeStore } from './recipeStore';
+import { useRecipeStore } from '../stores/recipeStore';
 
 const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
