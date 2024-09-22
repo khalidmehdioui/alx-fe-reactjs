fe4feb0f270edcc973e9495407844173f0cfe324
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-22 13:17:09 +0100

    Create Header.jsx

---
+++ github-user-search/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <div>
+        <h1>GitHub User Search</h1>
+        <p>Enter a GitHub username to see details</p>
+      </div>
+    </>
+  );
+}
