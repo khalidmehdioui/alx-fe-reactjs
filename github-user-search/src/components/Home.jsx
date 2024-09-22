4d4256a96fdda617cd5e1eb64a7b43cb1c60e8da
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-22 13:17:32 +0100

    Create Home.jsx

---
+++ github-user-search/src/components/Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the GitHub User Search App!</h2>
+      <p>Start searching for GitHub users by typing in the search bar.</p>
+    </div>
+  );
+};
+
+export default Home;
