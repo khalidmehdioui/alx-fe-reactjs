3f45e5102909ccb4ed387934a3e81c2db7353b16
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-22 13:08:37 +0100

    Update githubApi.js

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,7 +1,13 @@
+// src/services/githubAPI.js
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_API_BASE_URL = 'https://api.github.com';
 
-export const fetchUserData = (username) => {
-  return axios.get(`${GITHUB_API_URL}/${username}`);
+export const fetchGitHubUser = async (username) => {
+  const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}`, {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+    },
+  });
+  return response.data;
 };
