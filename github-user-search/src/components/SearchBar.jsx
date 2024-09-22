171b8d5ae41bdbaae5e115ac6fe656fdabb638e7
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-09-22 12:14:49 +0100

    Create SearchBar.jsx

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search GitHub User..." />
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
